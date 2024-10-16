7a5cf76ba993fb933d0953067d70a5374e50add9
ascaniio <ascaniozorzi@gmail.com>
2024-10-16 22:48:01 +0200

    creazione

--- index.php
+++ index.php
@@ -4,8 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+     <!-- CDN di Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
+
+
+    <link rel="stylesheet" href="css/css.css">
+
+     <!-- CDN di Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+  <!-- CDN di React -->
+  <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+
+  <!-- CDN di ReactDOM -->
+  <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+
+
+  <!-- Collegamento al file JavaScript con il tuo codice React -->
+  <script defer src="app.js"></script>
 </head>
 <body>
-    <h1>CiaoCiao</h1>
+    
+
+<!-- Div dove React inietterà il contenuto -->
+<div id="root"></div>
+
+
+<!-- Bootstrap JavaScript e dipendenze (opzionale per componenti interattivi) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
